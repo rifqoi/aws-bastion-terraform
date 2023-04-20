7df135ab34b315fb7db6e2d784903fc86287a9e6
rifqoi <alfurqon.rifqi@gmail.com>
2023-04-20 15:47:57 +0700

    add aws provider

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "Default region"
+}
+
+variable "zone" {
+  default     = "us-east-1a"
+  description = "Default AZ"
+}
+
+variable "instance-type" {
+  default     = "t3-micro"
+  description = "Default instance type of this project"
+}
